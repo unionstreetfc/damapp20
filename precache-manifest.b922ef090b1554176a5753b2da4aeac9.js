141d578fcd4f69bec49b7bfb1a7207047a9a5156
Union Street FC <finlay.birnie1@gmail.com>
2020-01-19 20:49:31 +0000

    Updates

---
+++ precache-manifest.b922ef090b1554176a5753b2da4aeac9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07596408a1a3a6620b83d898713d3811",
+    "url": "/damapp20/index.html"
+  },
+  {
+    "revision": "4a1114346f40c0101cbc",
+    "url": "/damapp20/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "791e999538f2a8715372",
+    "url": "/damapp20/static/css/main.f40a38a2.chunk.css"
+  },
+  {
+    "revision": "4a1114346f40c0101cbc",
+    "url": "/damapp20/static/js/2.74ea754f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/damapp20/static/js/2.74ea754f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "791e999538f2a8715372",
+    "url": "/damapp20/static/js/main.a6c77b2a.chunk.js"
+  },
+  {
+    "revision": "1ab71e74aa689c2e4a7f",
+    "url": "/damapp20/static/js/runtime-main.b5cb3b84.js"
+  },
+  {
+    "revision": "493246c7e16319e396688ddf0feddb95",
+    "url": "/damapp20/static/media/ben.493246c7.jpg"
+  },
+  {
+    "revision": "ef5080eb6fda222e4271d8562350610e",
+    "url": "/damapp20/static/media/darren.ef5080eb.jpg"
+  },
+  {
+    "revision": "ef26cf1782131ada85c7696afe7bfcbe",
+    "url": "/damapp20/static/media/diego.ef26cf17.jpg"
+  },
+  {
+    "revision": "53c1ce23e079e5a0bc98b7380dd8c83a",
+    "url": "/damapp20/static/media/eddy.53c1ce23.jpg"
+  },
+  {
+    "revision": "215219667736b9be6fbd4896fa3f600b",
+    "url": "/damapp20/static/media/finlay.21521966.jpg"
+  },
+  {
+    "revision": "200a33efc70349a9a5a96465d4acac4d",
+    "url": "/damapp20/static/media/keith.200a33ef.jpg"
+  },
+  {
+    "revision": "efa2f1021b31ea3e32f44816c70a8dcd",
+    "url": "/damapp20/static/media/mark.efa2f102.jpg"
+  },
+  {
+    "revision": "365530101deef2b2101a5942b09743b7",
+    "url": "/damapp20/static/media/mooney.36553010.jpg"
+  },
+  {
+    "revision": "61ae7e6afdec97c0f5722600a1e535d5",
+    "url": "/damapp20/static/media/pratch.61ae7e6a.jpg"
+  },
+  {
+    "revision": "473d6e53a78d30b000187bf8b9e3e83f",
+    "url": "/damapp20/static/media/smithy.473d6e53.jpeg"
+  }
+]);
