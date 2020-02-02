b4224f65462e87074ba627eeac28b8906da3237b
Union Street FC <finlay.birnie1@gmail.com>
2020-02-02 11:39:23 +0000

    add matt and ending screen

--- src/PersonCard.js
+++ src/PersonCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Button, Card, Modal } from 'react-bootstrap';
+import { Button, Card, Jumbotron, Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 import data from './data';
+
+import badgers from './badgers.jpg';
+import zero from './zero.png';
 
 class PersonCard extends React.Component {
   constructor() {
@@ -55,34 +58,57 @@ class PersonCard extends React.Component {
       width: '300px',
       height: '500px',
     }
-    return (
-      <div>
-        <Card style={cardStyle}>
-          <Card.Img variant="top" src={data[this.state.step].img} style={imageStyle}/>
-          <Card.Body>
-            <Card.Title>{data[this.state.step].name + ', ' + data[this.state.step].age}</Card.Title>
-            <Button
-              style={{float: 'left'}}
-              variant="light"
-              onClick={this.advance}>
-              <FontAwesomeIcon size="2x" style={{color: 'green'}} icon={faHeart}/>
-            </Button>
-            <Button
-              style={{float: 'right'}}
-              variant="light"
-              onClick={this.advance}>
-              <FontAwesomeIcon size="2x" style={{color: 'green'}} icon={faHeart}/>
-            </Button>
-          </Card.Body>
-        </Card>
-        <Modal show={this.state.showModal} onClick={this.hideModal} centered>
-          <Modal.Header>
-            <Modal.Title>It's a match!</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>{data[this.state.step].name + ' likes you!'}</Modal.Body>
-        </Modal>
-      </div>
-    );
+
+    let contents =  this.state.done ?
+      (
+        <Jumbotron style={{marginTop: '50px'}}>
+          <span>
+            <FontAwesomeIcon size="2x" style={{color: 'green', float: 'left'}} icon={faHeart}/>
+            <FontAwesomeIcon size="2x" style={{color: 'green', float: 'right'}} icon={faHeart}/>
+          </span>
+          <h1>It's a Match!</h1>
+          <h2 style={{marginBottom: '50px'}}>Kullen Nullen Aachen and Union Street Oxford</h2>
+          <p>
+            <img src={badgers} height='100px' width='100px' style={{float: 'left', marginRight: '30px'}} />
+            <FontAwesomeIcon size="2x" style={{color: 'green'}} icon={faHeart}/>
+            <img src={zero} height='100px' width='100px' style={{float: 'right', marginLeft: '30px'}} />
+          </p>
+          <p style={{clear: 'both'}}>Please consider our offering of Love.</p>
+          <p>We hope to enjoy more matches with you in Landgraaf.</p>
+          <p>Finally, don't forget to <a href="https://scratch.mit.edu/projects/361354173/">play some football</a>.</p>
+        </Jumbotron>
+      )
+    :
+      (
+        <div>
+          <Card style={cardStyle}>
+            <Card.Img variant="top" src={data[this.state.step].img} style={imageStyle}/>
+            <Card.Body>
+              <Card.Title>{data[this.state.step].name + ', ' + data[this.state.step].age}</Card.Title>
+              <Button
+                style={{float: 'left'}}
+                variant="light"
+                onClick={this.advance}>
+                <FontAwesomeIcon size="2x" style={{color: 'green'}} icon={faHeart}/>
+              </Button>
+              <Button
+                style={{float: 'right'}}
+                variant="light"
+                onClick={this.advance}>
+                <FontAwesomeIcon size="2x" style={{color: 'green'}} icon={faHeart}/>
+              </Button>
+            </Card.Body>
+          </Card>
+          <Modal show={this.state.showModal} onClick={this.hideModal} centered>
+            <Modal.Header>
+              <Modal.Title>It's a match!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{data[this.state.step].name + ' likes you!'}</Modal.Body>
+          </Modal>
+        </div>
+      )
+
+    return contents;
   }
 }
 
